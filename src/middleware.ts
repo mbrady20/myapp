b48fb76de132c3b8b3c2b7d468fcbcc34370ed9c
mbrady20 <bradymichael362@gmail.com>
2023-08-21 17:06:27 -0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   // "/" will be accessible to all users
-
-  afterAuth() {
-  },
+  
   publicRoutes: ["/"]
   
 });
