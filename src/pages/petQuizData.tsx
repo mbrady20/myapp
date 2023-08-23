a135072a33d13690968474a6566731599e67ea38
mbrady20 <bradymichael362@gmail.com>
2023-08-23 12:58:26 -0500

    Update petQuizData.tsx

--- src/pages/petQuizData.tsx
+++ src/pages/petQuizData.tsx
@@ -1,14 +1,16 @@
-import { Card, Container } from "@chakra-ui/react";
+import { Button, Card, Container } from "@chakra-ui/react";
 import { api } from "npm/utils/api";
 
 export default function PetQuizData() {
 
     const { data } = api.example.getAll.useQuery();
+
+    function func()  {
+        console.log(data);
+    }
     return(
         <Container bg="blue.50" minWidth={"100vw"} minHeight={"100vh"}>
-            <Card>
-{/*                {data?.map(data.)}
- */}            </Card>
+            <Button onClick={func}></Button>
         </Container>
     )
 }
