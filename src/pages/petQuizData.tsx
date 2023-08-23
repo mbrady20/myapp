305b6637c4f8f13d91cad58b1b501e1eda718e90
mbrady20 <bradymichael362@gmail.com>
2023-08-23 13:30:21 -0500

    Update petQuizData.tsx

--- src/pages/petQuizData.tsx
+++ src/pages/petQuizData.tsx
@@ -1,26 +1,28 @@
 import { Button, Card, Container } from "@chakra-ui/react";
 import { api } from "npm/utils/api";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function PetQuizData() {
 
     const { data } = api.example.getAll.useQuery();
-    const [sydRank, setSydRank] = useState(0); 
-    const [lokRank, setLokRank] = useState(0); 
-    const [stuRank, setStuRank] = useState(0); 
-    const [elRank, setElRank] = useState(0); 
+    const [sydRank, setSydRank] = useState(0);
+    const [lokRank, setLokRank] = useState(0);
+    const [stuRank, setStuRank] = useState(0);
+    const [elRank, setElRank] = useState(0);
     const [totRank, setTotRank] = useState(0);
-      
-        useEffect(() => {
-            data?.forEach((element) => {
-                setTotRank(totRank + 4);
-                setSydRank(5 - element.sydneyRank + sydRank);
-                setLokRank(5 - element.lokiRank + lokRank);
-                setStuRank(5 - element.stuartRank + stuRank);
-                setElRank(5 - element.elGatoRank + elRank);
+
+    useEffect(() => {
+        data?.forEach((element) => {
+            setTotRank(totRank + 4);
+            setSydRank(5 - element.sydneyRank + sydRank);
+            setLokRank(5 - element.lokiRank + lokRank);
+            setStuRank(5 - element.stuartRank + stuRank);
+            setElRank(5 - element.elGatoRank + elRank);
             });
-        }, [window]);
- 
+            
+    }, [window] );
+
+    
 
     function func()  {
         console.log(data);
@@ -30,8 +32,4 @@ export default function PetQuizData() {
             <Button onClick={func}></Button>
         </Container>
     )
-}
-
-function useEffect(arg0: () => void, arg1: (Window & typeof globalThis)[]) {
-    throw new Error("Function not implemented.");
 }
