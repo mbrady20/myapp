2ec5fefe272e4cee2cd259946ee683c05464782b
mbrady20 <bradymichael362@gmail.com>
2023-08-25 16:05:50 -0500

    Update petQuizData.tsx

--- src/pages/petQuizData.tsx
+++ src/pages/petQuizData.tsx
@@ -6,9 +6,12 @@ import {
   Center,
   Container,
   Divider,
+  Flex,
   Grid,
   GridItem,
   Icon,
+  Image,
+  SimpleGrid,
   Table,
   TableCaption,
   TableContainer,
@@ -66,6 +69,10 @@ export default function PetQuizData() {
   const [stuRankOne, setStuRankOne] = useState(0);
   const [elRankOne, setElRankOne] = useState(0);
   const [totCount, setTotCount] = useState(0);
+  const [sydPSum, setSydPsum] = useState(0);
+  const [lokPSum, setLokPsum] = useState(0);
+  const [stuPSum, setStuPsum] = useState(0);
+  const [elPSum, setElPsum] = useState(0);
 
   const [answer, setAnswer] = useRecoilState(answerState);
 
@@ -79,6 +86,11 @@ export default function PetQuizData() {
   let lokOneCount = 0;
   let stuOneCount = 0;
   let elOneCount = 0;
+
+  let sydAvNum = 0;
+  let lokAvNum = 0;
+  let stuAvNum = 0;
+  let elAvNum = 0;
   useEffect(() => {
     data?.forEach(
       (element: {
@@ -92,6 +104,11 @@ export default function PetQuizData() {
         lokCount = 5 - element.lokiRank + lokCount;
         stuCount = 5 - element.stuartRank + stuCount;
         elCount = 5 - element.elGatoRank + elCount;
+
+        sydAvNum += element.sydneyRank;
+        lokAvNum += element.lokiRank;
+        stuAvNum += element.stuartRank;
+        elAvNum += element.elGatoRank;
 
         if (element.sydneyRank.valueOf() == 1) ++sydOneCount;
         if (element.lokiRank.valueOf() == 1) ++lokOneCount;
@@ -108,6 +125,11 @@ export default function PetQuizData() {
     setStuRankOne(stuOneCount);
     setElRankOne(elOneCount);
     setTotCount(count);
+
+    setSydPsum(sydAvNum);
+    setStuPsum(stuAvNum);
+    setLokPsum(lokAvNum);
+    setElPsum(elAvNum);
   }, [data]);
 
   const barChartData = [
@@ -148,6 +170,53 @@ export default function PetQuizData() {
     },
   ];
 
+  const userResultImages = [
+    {
+        index: answer.syd,
+        value: "/sydney2.png",
+        name: "Sydney"
+    },
+    {
+        index: answer.lok,
+        value: "/loki.jpeg",
+        name: "Loki"
+    },
+    {
+        index: answer.stu,
+        value: "/stuart.png",
+        name: "Stuart"
+    },
+    {
+        index: answer.el,
+        value: "/elgato.png",
+        name: "El Gato"
+    }
+  ]
+
+
+  const averageResultImages = [
+    {
+        index: sydPSum / totCount,
+        value: "/sydney2.png",
+        name: "Sydney"
+    },
+    {
+        index: lokPSum / totCount,
+        value: "/loki.jpeg",
+        name: "Loki"
+    },
+    {
+        index: stuPSum / totCount,
+        value: "/stuart.png",
+        name: "Stuart"
+    },
+    {
+        index: elPSum / totCount,
+        value: "/elgato.png",
+        name: "El Gato"
+    }
+  ]
+
   const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 
   const RADIAN = Math.PI / 180;
@@ -166,18 +235,18 @@ export default function PetQuizData() {
     const x: number = props.cx + radius * Math.cos(-props.midAngle * RADIAN);
     const y: number = props.cy + radius * Math.sin(-props.midAngle * RADIAN);
 
-    if(props.percent.valueOf() != 0)
-    return (
-      <text
-        x={x}
-        y={y}
-        fill="white"
-        textAnchor={x > props.cx ? "start" : "end"}
-        dominantBaseline="central"
-      >
-        {` ${props.name} ${(props.percent * 100).toFixed(0)}% `}
-      </text>
-    );
+    if (props.percent.valueOf() != 0)
+      return (
+        <text
+          x={x}
+          y={y}
+          fill="white"
+          textAnchor={x > props.cx ? "start" : "end"}
+          dominantBaseline="central"
+        >
+          {` ${props.name} ${(props.percent * 100).toFixed(0)}% `}
+        </text>
+      );
   };
 
   function resultsButtonClick() {
@@ -199,7 +268,8 @@ export default function PetQuizData() {
   function recentButtonClick() {
     setViewMode1(false);
     setViewMode2(false);
-    console.log(data2);
+    
+    
   }
 
   return (
@@ -266,10 +336,47 @@ export default function PetQuizData() {
         </Container>
       </GridItem>
       {viewMode1 && viewMode2 && (
+   
         <GridItem colSpan={9} height={"100vh"} bg="green.50">
-                   <Text>Sydney: {answer.syd}</Text>
+            <Center>
+                        <Text height={"10vh"} paddingTop={"10px"} as="b" fontSize={"3xl"}>
+              Your Ranking!
+            </Text>
+            </Center>
+          <SimpleGrid columns={4} paddingLeft={"80px"}>
+            {userResultImages.sort((a, b) => a.index - b.index).map((element) => (
+            <GridItem key = {element.index} height={"100%"} width={"50%"}>
+            <Image
+              src={element.value}
+              width={"100%"}
+              height={"100%"}
+              alt="Sydney"
+            ></Image>
+            <Text>{element.name}: {element.index}</Text>
+            </GridItem>
+            
+            ))}
+          </SimpleGrid>
+<Center>
+          <Text height={"10vh"} paddingTop={"30px"} as="b" fontSize={"3xl"}>
+              Average Ranking!
+            </Text>
+            </Center>
+          <SimpleGrid columns={4} paddingLeft={"80px"} paddingTop={"20px"}>
+            {averageResultImages.sort((a, b) => a.index - b.index).map((element) => (
+            <GridItem key = {element.index} height={"100%"} width={"50%"}>
+            <Image
+              src={element.value}
+              width={"100%"}
+              height={"100%"}
+              alt="Sydney"
+            ></Image>
+            <Text>{element.name}: {element.index}</Text>
+            </GridItem>
+            
+            ))}
+          </SimpleGrid>
         </GridItem>
- 
       )}
       {viewMode1 && !viewMode2 && (
         <GridItem colSpan={9} height={"100vh"} bg="red.50">
@@ -278,7 +385,7 @@ export default function PetQuizData() {
               Percentage of vote share for each pet
             </Text>
           </Center>
-          <Container height={"90vh"} minWidth={"80vw"} >
+          <Container height={"90vh"} minWidth={"80vw"}>
             <ResponsiveContainer>
               <BarChart data={barChartData}>
                 <XAxis
@@ -309,7 +416,7 @@ export default function PetQuizData() {
               Percentage of voters who voted each pet the cutest
             </Text>
           </Center>
-          <Container height={"90vh"} minWidth={"80vw"} >
+          <Container height={"90vh"} minWidth={"80vw"}>
             <ResponsiveContainer width="100%" height="100%">
               <PieChart width={400} height={400}>
                 <Pie
@@ -336,44 +443,32 @@ export default function PetQuizData() {
 
       {!viewMode1 && !viewMode2 && (
         <GridItem colSpan={9} height={"100vh"} bg="purple.50">
-            <TableContainer>
-                <Table variant = 'simple'>
-                    <TableCaption>
-                        Ten most recent Pet Quiz results
-                    </TableCaption>
+          <TableContainer>
+            <Table variant="simple">
+              <TableCaption>Ten most recent Pet Quiz results</TableCaption>
 
-                    <Thead>
-                        <Tr>
-                            <Th>Initials</Th>
-                            <Th>Sydney Rank</Th>
-                            <Th>Loki Rank</Th>
-                            <Th>Stuart Rank</Th>
-                            <Th>El Gato Rank</Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                        {data2?.map((element) => 
-                        <Tr key={element.id}>
-                            <Td>
-                                {element.initials}
-                            </Td>
-                            <Td>
-                                {element.sydneyRank}
-                            </Td>
-                            <Td>
-                                {element.lokiRank}
-                            </Td>
-                            <Td>
-                                {element.stuartRank}
-                            </Td>
-                            <Td>
-                                {element.elGatoRank}
-                            </Td>
-                        </Tr>)}
-                    </Tbody>
-                </Table>
-            </TableContainer>
-
+              <Thead>
+                <Tr>
+                  <Th>Initials</Th>
+                  <Th>Sydney Rank</Th>
+                  <Th>Loki Rank</Th>
+                  <Th>Stuart Rank</Th>
+                  <Th>El Gato Rank</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {data2?.map((element) => (
+                  <Tr key={element.id}>
+                    <Td>{element.initials}</Td>
+                    <Td>{element.sydneyRank}</Td>
+                    <Td>{element.lokiRank}</Td>
+                    <Td>{element.stuartRank}</Td>
+                    <Td>{element.elGatoRank}</Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
         </GridItem>
       )}
     </Grid>
